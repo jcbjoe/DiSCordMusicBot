e89135253d53ab3e583a7f7cd53a6fa4c8abc8e2
Joe <joegarlick28@hotmail.com>
2020-04-12 19:20:25 +0100

    Start

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ytdl = require('ytdl-core');
+
+client.login('Njk4OTUzODYxOTc2Njg2NzM0.XpNVmQ.mXA6Gzvo2ctF103ymJ9iccVO7-8');
+
+let stream = null;
+
+client.on('message', async message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content.startsWith('!play')) {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+
+        const messageArgs = message.content.match(/(?:[^\s"]+|"[^"]*")+/g);
+        console.log(messageArgs);
+
+      stream = connection.play(ytdl(messageArgs[1], { filter: 'audioonly' }));
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+
+  if (message.content.startsWith('!vol')) {
+    const messageArgs = message.content.match(/(?:[^\s"]+|"[^"]*")+/g);
+
+    stream.setVolume(messageArgs[1] / 100);
+  }
+});
